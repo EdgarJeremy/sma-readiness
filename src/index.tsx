0901bb34885c80af557bcf8b017bbf1b2f0647b6
EdgarJeremy <edgarpontoh3141@gmail.com>
2025-01-24 12:41:25 +0800

    (*) fix weekdays error

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import './styles/main.css'
+import dayjs from 'dayjs'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import localeData from 'dayjs/plugin/localeData'
+import weekday from 'dayjs/plugin/weekday'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import weekYear from 'dayjs/plugin/weekYear'
+
+dayjs.extend(customParseFormat)
+dayjs.extend(advancedFormat)
+dayjs.extend(weekday)
+dayjs.extend(localeData)
+dayjs.extend(weekOfYear)
+dayjs.extend(weekYear)
 
 import App from "./App";
 
